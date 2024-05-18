2f614a56e1fedf1142b5dcc26f772117615df71d
Eric Brechner <ericbrec@msn.com>
2024-05-17 17:00:21 -0700

    Introduced matrix derivative to modeler and robots

--- modeler.py
+++ modeler.py
@@ -5,24 +5,33 @@ class Modeler:
     def __init__(self):
         self.matrixStack = []
         self.matrix = np.identity(4)
+        self.dMatrix = np.zeros((4, 4))
+        self.dMatrix[3,3] = 1.0
     
     def __call__(self):
         return self.matrix
     
     def pop(self):
         if self.matrixStack:
-            self.matrix = self.matrixStack.pop()
+            self.matrix, self.dMatrix = self.matrixStack.pop()
         else:
             self.matrix = np.identity(4)
+            self.dMatrix = np.zeros((4, 4))
+            self.dMatrix[3,3] = 1.0
 
     def push(self):
-        self.matrixStack.append(self.matrix)    
+        self.matrixStack.append((self.matrix, self.dMatrix))
 
-    def rotate(self, axis, radians):
-        self.matrix = self.matrix @ self.rotation(axis, radians)    
+    def multiply(self, matrix, dMatrix):
+        self.matrix = self.matrix @ matrix
+        self.dMatrix[:3,:3] = self.dMatrix[:3,:3] @ matrix[:3,:3] + self.matrix[:3,:3] @ dMatrix[:3,:3]
+        self.dMatrix[:3,3] += self.dMatrix[:3,:3] @ matrix[:3, 3] + self.matrix[:3,:3] @ dMatrix[:3, 3]
+
+    def rotate(self, axis, radians, dRadians=0.0):
+        self.multiply(*self.rotation(axis, radians, dRadians))
     
     @staticmethod
-    def rotation(axis, radians):
+    def rotation(axis, radians, dRadians=0.0):
         indices = [
             [[1, 1, 2, 2], [1, 2, 1, 2]],
             [[0, 2, 0, 2], [0, 0, 2, 2]],
@@ -30,19 +39,28 @@ class Modeler:
         ]
         matrix = np.identity(4)
         matrix[indices[axis][0], indices[axis][1]] = np.array((np.cos(radians), -np.sin(radians), np.sin(radians), np.cos(radians)))
-        return matrix
 
-    def scale(self, *args):
-        self.matrix = self.matrix @ self.scaling(*args)    
+        dMatrix = np.zeros((4, 4))
+        dMatrix[3,3] = 1.0
+        dMatrix[indices[axis][0], indices[axis][1]] = dRadians * np.array((-np.sin(radians), -np.cos(radians), np.cos(radians), -np.sin(radians)))
+
+        return matrix, dMatrix
+
+    def scale(self, v, dV=(0.0, 0.0, 0.0)):
+        self.multiply(*self.scaling(v, dV))
     
     @staticmethod
-    def scaling(*args):
-        vector = args if np.isscalar(args[0]) else args[0]
-        count = min(len(vector), 3)
+    def scaling(v, dV=(0.0, 0.0, 0.0)):
         matrix = np.identity(4)
-        for i in range(count):
-            matrix[i, i] = vector[i]
-        return matrix
+        for i in range(min(len(v), 3)):
+            matrix[i, i] = v[i]
+
+        dMatrix = np.zeros((4, 4))
+        dMatrix[3,3] = 1.0
+        for i in range(min(len(dV), 3)):
+            dMatrix[i, i] = dV[i]
+
+        return matrix, dMatrix
 
     def transform(self, *args):
         values = args if np.isscalar(args[0]) else args[0]
@@ -50,15 +68,20 @@ class Modeler:
             matrix = self.matrix[:3, :3]
             matrixInverseTranspose = np.transpose(np.linalg.inv(matrix))
             translation = self.matrix[:3, 3]
+            def transformManifold(manifold):
+                transformedManifold = manifold.transform(matrix, matrixInverseTranspose).translate(translation)
+                transformedManifold._dP = self.dMatrix[:3, :3] @ manifold.evaluate((0.0, 0.0)) + self.dMatrix[:3, 3]
+                return transformedManifold
+            
             if isinstance(values, Solid):
                 solid = Solid(values.dimension, values.containsInfinity)
                 for boundary in values.boundaries:
-                    solid.add_boundary(Boundary(boundary.manifold.transform(matrix, matrixInverseTranspose).translate(translation), boundary.domain))
+                    solid.add_boundary(Boundary(transformManifold(boundary.manifold), boundary.domain))
                 return solid
             elif isinstance(values, Boundary):
-                return Boundary(values.manifold.transform(matrix, matrixInverseTranspose).translate(translation), values.domain)
+                return Boundary(transformManifold(values.manifold), values.domain)
             else:
-                return values.transform(matrix, matrixInverseTranspose).translate(translation)
+                return transformManifold(values)
         else:
             count = min(len(values), 3)
             vector = np.zeros(4)
@@ -66,13 +89,18 @@ class Modeler:
             vector[3] = 1.0
             return self.matrix @ vector
 
-    def translate(self, *args):
-        self.matrix = self.matrix @ self.translation(*args)
+    def translate(self, v, dV=(0.0, 0.0, 0.0)):
+        self.multiply(*self.translation(v, dV))
         
     @staticmethod
-    def translation(*args):
-        vector = args if np.isscalar(args[0]) else args[0]
-        count = min(len(vector), 3)
+    def translation(v, dV=(0.0, 0.0, 0.0)):
+        count = min(len(v), 3)
         matrix = np.identity(4)
-        matrix[:count, 3] = vector[:count]
-        return matrix
+        matrix[:count, 3] = v[:count]
+
+        count = min(len(dV), 3)
+        dMatrix = np.zeros((4, 4))
+        dMatrix[3,3] = 1.0
+        dMatrix[:count, 3] = dV[:count]
+
+        return matrix, dMatrix
--- robot.py
+++ robot.py
@@ -6,10 +6,25 @@ def add_boundaries_to_robot(robot, solid):
     for boundary in solid.boundaries:
         robot.add_boundary(boundary)
 
-def create_robot(position, hips, shoulder, elbow, wrist, bite, robot = None):
+def create_robot(parameters, t, robot = None):
     if robot is None:
         robot = Solid(3, False)
     
+    h = 0.0001
+    if callable(parameters):
+        position, hips, shoulder, elbow, wrist, bite = parameters(t)
+        dPosition, dHips, dShoulder, dElbow, dWrist, dBite = parameters(t + h)
+    else:
+        position, hips, shoulder, elbow, wrist, bite = parameters
+        dPosition, dHips, dShoulder, dElbow, dWrist, dBite = parameters
+
+    dPosition = (np.array(dPosition) - np.array(position)) / h
+    dHips = (dHips - hips) / h
+    dShoulder = (dShoulder - shoulder) / h
+    dElbow = (dElbow - elbow) / h
+    dWrist = (dWrist - wrist) / h
+    dBite = (dBite - bite) / h
+
     modeler = Modeler()
     base = Hyperplane.create_hypercube(((-2.0, 2.0),)*3)
     pivot = Hyperplane.create_hypercube(((-1.0, 1.0),)*3)
@@ -18,35 +33,35 @@ def create_robot(position, hips, shoulder, elbow, wrist, bite, robot = None):
     tooth = Hyperplane.create_hypercube(((-0.5, 0.5), (-0.2, 0.2), (0.0, 1.5)))
 
     modeler.rotate(0, -np.pi / 2)
-    modeler.translate(position)
+    modeler.translate(position, dPosition)
     add_boundaries_to_robot(robot, modeler.transform(base))
-    modeler.translate(0.0, 0.0, 3.0)
-    modeler.rotate(2, hips)
+    modeler.translate((0.0, 0.0, 3.0))
+    modeler.rotate(2, hips, dHips)
     add_boundaries_to_robot(robot, modeler.transform(pivot))
-    modeler.translate(1.0, 0.0, 1.0)
-    modeler.rotate(0, shoulder)
+    modeler.translate((1.0, 0.0, 1.0))
+    modeler.rotate(0, shoulder, dShoulder)
     add_boundaries_to_robot(robot, modeler.transform(arm))
-    modeler.translate(-0.5, 0.0, 4.0)
-    modeler.rotate(0, elbow)
+    modeler.translate((-0.5, 0.0, 4.0))
+    modeler.rotate(0, elbow, dElbow)
     modeler.push()
-    modeler.scale(0.5, 1.0, 1.0)
+    modeler.scale((0.5, 1.0, 1.0))
     add_boundaries_to_robot(robot, modeler.transform(pivot))
     modeler.pop()
-    modeler.translate(-1.5, 0.0, 0.0)
+    modeler.translate((-1.5, 0.0, 0.0))
     add_boundaries_to_robot(robot, modeler.transform(arm))
-    modeler.translate(0.5, 0.0, 5.5)
-    modeler.rotate(2, wrist)
+    modeler.translate((0.5, 0.0, 5.5))
+    modeler.rotate(2, wrist, dWrist)
     modeler.push()
-    modeler.scale(0.5, 0.5, 0.5)
+    modeler.scale((0.5, 0.5, 0.5))
     add_boundaries_to_robot(robot, modeler.transform(pivot))
     modeler.pop()
-    modeler.translate(0.0, 0.0, 0.5)
+    modeler.translate((0.0, 0.0, 0.5))
     add_boundaries_to_robot(robot, modeler.transform(jaw))
     modeler.push()
-    modeler.translate(0.0, bite, 0.5)
+    modeler.translate((0.0, bite, 0.5), (0.0, dBite, 0.0))
     add_boundaries_to_robot(robot, modeler.transform(tooth))
     modeler.pop()
-    modeler.translate(0.0, -bite, 0.5)
+    modeler.translate((0.0, -bite, 0.5), (0.0, -dBite, 0.0))
     add_boundaries_to_robot(robot, modeler.transform(tooth))
 
     return robot
@@ -68,6 +83,69 @@ def robot2_parameters(t):
     wrist = (1 - t) * 0 + t * np.pi / -2
     bite = 1.0 if t < 0.5 else (2 - 2 * t) * 1.0 + (2 * t - 1) * 0.5
     return (position, hips, shoulder, elbow, wrist, bite)
+
+def extrude_solid(solid, path):
+    assert len(path) > 1
+    assert solid.dimension+1 == len(path[0])
+    
+    extrusion = Solid(solid.dimension+1, False)
+
+    # Extrude boundaries along the path
+    point = None
+    for nextPoint in path:
+        nextPoint = np.atleast_1d(nextPoint)
+        if point is None:
+            point = nextPoint
+            continue
+        tangent = nextPoint - point
+        extent = tangent[solid.dimension]
+        tangent = tangent / extent
+        # Extrude each boundary
+        for boundary in solid.boundaries:
+            # Construct a normal orthogonal to both the boundary tangent space and the path tangent
+            extruded_normal = np.full((extrusion.dimension), 0.0)
+            extruded_normal[0:solid.dimension] = boundary.manifold._normal[:]
+            extruded_normal[solid.dimension] = -np.dot(boundary.manifold._normal, tangent[0:solid.dimension])
+            extruded_normal = extruded_normal / np.linalg.norm(extruded_normal)
+            # Construct a point that adds the boundary point to the path point
+            extruded_point = np.full((extrusion.dimension), 0.0)
+            extruded_point[0:solid.dimension] = boundary.manifold._point[:]
+            extruded_point += point
+            # Combine the boundary tangent space and the path tangent
+            extruded_tangentSpace = np.full((extrusion.dimension, solid.dimension), 0.0)
+            if solid.dimension > 1:
+                extruded_tangentSpace[0:solid.dimension, 0:solid.dimension-1] = boundary.manifold._tangentSpace[:,:]
+            extruded_tangentSpace[:, solid.dimension-1] = tangent[:]
+            extrudedHyperplane = Hyperplane(extruded_normal, extruded_point, extruded_tangentSpace)
+            # Construct a domain for the extruded boundary
+            if boundary.domain.dimension > 0:
+                # Extrude the boundary's domain to include path domain
+                domainPath = []
+                domainPoint = np.full((solid.dimension), 0.0)
+                domainPath.append(domainPoint)
+                domainPoint = np.full((solid.dimension), 0.0)
+                domainPoint[solid.dimension-1] = extent
+                domainPath.append(domainPoint)
+                extrudedDomain = extrude_solid(boundary.domain, domainPath)
+            else:
+                extrudedDomain = Solid(solid.dimension, False)
+                extrudedDomain.boundaries.append(Boundary(hyperplane_1D(-1.0, 0.0), Solid(0, True)))
+                extrudedDomain.boundaries.append(Boundary(hyperplane_1D(1.0, extent), Solid(0, True)))
+            # Add extruded boundary
+            extrusion.boundaries.append(Boundary(extrudedHyperplane, extrudedDomain))
+        
+        # Move onto the next point
+        point = nextPoint
+
+    # Add end cap boundaries
+    extrudedHyperplane = Hyperplane.create_axis_aligned(extrusion.dimension, solid.dimension, 0.0, True)
+    extrudedHyperplane = extrudedHyperplane.translate(path[0])
+    extrusion.boundaries.append(Boundary(extrudedHyperplane, solid))
+    extrudedHyperplane = Hyperplane.create_axis_aligned(extrusion.dimension, solid.dimension, 0.0, False)
+    extrudedHyperplane = extrudedHyperplane.translate(path[-1])
+    extrusion.boundaries.append(Boundary(extrudedHyperplane, solid))
+
+    return extrusion
 
 """
 def add_time_dimension(solidFunction, t1, t2, samples):
@@ -138,9 +216,10 @@ def add_time_dimension(solidFunction, t1, t2, samples):
 
 if __name__ == "__main__":
     viewer = Viewer()
+    viewer.set_background_color(np.array((1, 1, 1, 1),np.float32))
     for t in np.linspace(0.0, 1.0, 11):
         robot = None
-        robot = create_robot(*robot1_parameters(t))
-        robot = create_robot(*robot2_parameters(t), robot)
-        viewer.list(robot)
+        robot = create_robot(robot1_parameters, t)
+        robot = create_robot(robot2_parameters, t, robot)
+        viewer.list(robot, fillColor=np.array((0, 1, 0, 1),np.float32))
     viewer.mainloop()
